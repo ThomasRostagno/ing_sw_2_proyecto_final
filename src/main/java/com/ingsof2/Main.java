df795db33de8d6ad01c175d37587750ed214026e
ThomasRostagno <trostagno@hotmail.com>
2020-11-19 20:28:40 -0300

    cambio singleton

--- src/main/java/com/ingsof2/Main.java
+++ src/main/java/com/ingsof2/Main.java
@@ -1,6 +1,6 @@
 package com.ingsof2;
 
-import com.ingsof2.database.SDatabase;
+import com.ingsof2.database.Database;
 import com.ingsof2.frames.MainFrame;
 
 import java.sql.Connection;
@@ -11,37 +11,21 @@ public class Main {
 
     public static MainFrame mainFrame;
 
-    public static void main(String[] args) {//sacar throws
-
+    public static void main(String[] args) {
 
         //mainFrame = new MainFrame();
 
-        //Connection auxiliar = SDatabase.getInstance();
-        /*SDatabase sdbConn = SDatabase.getInstance();
-        sdbConn.getConn();*/
-        Connection sdbConn = SDatabase.getInstance().getConn();
-        //Connection connection = Database.INSTANCE.connect();//ESTA NO CONECTA
-
-        //Connection con = (Connection) SDatabase.getInstance();
-        /*SDatabase aux = new SDatabase();
-        aux = aux.getInstance();*/
-
-
-        //Connection connection = Database.INSTANCE.connect();
+        Connection sdbConn = Database.getInstance().getConn();
 
         Statement statement;
         try {
             statement = sdbConn.createStatement();
-            //statement.executeUpdate("DROP TABLE STUDENT");
             statement.executeUpdate("CREATE TABLE STUDENT(ID INT NOT NULL, NAME VARCHAR)");
             statement.executeUpdate("DROP TABLE STUDENT");
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-            //Main.mainFrame.showException(new ApiException(ErrorCode.FAIL_EXECUTING_QUERY));
         }
 
-        SDatabase.getInstance().disconnect();
-
-        //Database.INSTANCE.disconnect(connection);//*/
+        Database.getInstance().disconnect();
     }
 }
--- src/main/java/com/ingsof2/database/Database.java
+++ src/main/java/com/ingsof2/database/Database.java
@@ -7,15 +7,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public enum Database {
-    INSTANCE;
+public class Database {
+    private static Database INSTANCE;
+    private Connection conn;
+    private String url = "jdbc:postgresql://34.95.188.39:5432/";
+    private String user = "postgres";
+    private String password = "951753";
 
-    private final String url = "jdbc:postgresql://34.95.188.39:5432/";
-    private final String user = "postgres";
-    private final String password = "951753";
-
-    public Connection connect() throws ApiException {
-        Connection conn = null;
+    public Database() {
         try {
             conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connected to the PostgreSQL server successfully.");
@@ -23,13 +22,32 @@ public enum Database {
             System.out.println(e.getMessage());
             ApiException.showException(new ApiException(ErrorCode.FAIL_CONNECTING_TO_DB));
         }
+    }
 
+    private static void createInstance() {
+        try {
+            if (INSTANCE == null || INSTANCE.getConn().isClosed()) {
+                INSTANCE = new Database();
+            }
+        } catch (SQLException throwables) {
+            ApiException.showException(new ApiException(ErrorCode.FAIL_CONNECTING_TO_DB));
+        }
+    }
+
+    public static Database getInstance() {
+        if (INSTANCE == null) {
+            createInstance();
+        }
+        return INSTANCE;
+    }
+
+    public Connection getConn() {
         return conn;
     }
 
-    public void disconnect(Connection connection) throws ApiException {
+    public void disconnect() {
         try {
-            connection.close();
+            conn.close();
             System.out.println("Disconnected from the PostgreSQL server successfully.");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
