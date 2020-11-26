120b4600c9c2b681c0d7a7ea4edeb5919276f925
Daniel Kwist <kwistdaniel@gmail.com>
2020-11-26 17:01:09 -0300

    Headers Tablas

--- src/main/java/com/ingsof2/Objetos/Zona.java
+++ src/main/java/com/ingsof2/Objetos/Zona.java
@@ -1,5 +1,7 @@
 package com.ingsof2.Objetos;
 
+import java.util.List;
+
 public class Zona {
     private String codigo;
     private String nombre;
@@ -42,5 +44,22 @@ public class Zona {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+
+    public Object[][] getDataVector(List<Zona> zonas) {
+        Object[][] objects = new Object[0][0];
+
+        for (int i = 0; i < zonas.size(); i++) {
+            objects[i] = zonas.get(i).toObject();
+        }
+        return objects;
+    }
+
+    private Object[] toObject() {
+        return new Object[]{getCodigo(),getNombre(),getDescripcion()};
+    }
+
+    public Object[] getHeaders() {
+        return new Object[]{"Codigo", "Nombre", "Descripcion"};
     }
 }
