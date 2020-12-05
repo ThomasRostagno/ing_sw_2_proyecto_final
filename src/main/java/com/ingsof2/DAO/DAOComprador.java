837704a66b94d86592cd4f1a2a0a39fc0d1693db
Daniel Kwist <kwistdaniel@gmail.com>
2020-12-05 18:47:54 -0300

    Venta y Vendedor

---
+++ src/main/java/com/ingsof2/DAO/DAOComprador.java
@@ -0,0 +1,138 @@
+package com.ingsof2.DAO;
+
+import com.ingsof2.Objetos.Comprador;
+import com.ingsof2.Objetos.Inquilino;
+import com.ingsof2.database.Database;
+import com.ingsof2.exceptions.ApiException;
+import com.ingsof2.utils.ErrorCode;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOComprador implements BusinessObject<Comprador>{
+    @Override
+    public List<Comprador> readAll() {
+        List<Comprador> compradores = new ArrayList<>();
+        Connection connection = Database.getInstance().getConnection();
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Comprador WHERE (Status=1)");
+            Comprador comprador;
+            while (rs.next()) {
+                comprador = new Comprador();
+                comprador.setNombre(rs.getString("Nombre"));
+                comprador.setApellido(rs.getString("Apellido"));
+                comprador.setTelefono(rs.getString("Telefono"));
+                comprador.setDni(rs.getString("DNI"));
+                comprador.setSexo(rs.getString("Sexo"));
+                comprador.setDireccion(rs.getString("Direccion"));
+                comprador.setFechaNac(rs.getString("Fecha_Nacimiento"));
+                comprador.setEmail(rs.getString("Email"));
+                compradores.add(comprador);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        Database.getInstance().disconnect();
+        return compradores;
+    }
+
+    @Override
+    public Comprador ReadOne(String... ids) {
+        Comprador comprador = new Comprador();
+        Connection connection = Database.getInstance().getConnection();
+        Statement statement;
+        try{
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Comprador WHERE (Status=1) AND (DNI='"+ids[0]+"') AND (Sexo='"+ids[1]+"')");
+            while(rs.next()){
+                comprador.setNombre(rs.getString("Nombre"));
+                comprador.setApellido(rs.getString("Apellido"));
+                comprador.setTelefono(rs.getString("Telefono"));
+                comprador.setDni(rs.getString("DNI"));
+                comprador.setSexo(rs.getString("Sexo"));
+                comprador.setDireccion(rs.getString("Direccion"));
+                comprador.setFechaNac(rs.getString("Fecha_Nacimiento"));
+                comprador.setEmail(rs.getString("Email"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return comprador;
+    }
+
+    @Override
+    public int create(Comprador comprador) {
+        String sqlInsert = " INSERT INTO Comprador (Nombre, Apellido, Telefono, DNI, Sexo, Direccion, Fecha_Nacimiento, Email, Status)" +
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        int exito = 0;
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(sqlInsert);
+            statement.setString(1, comprador.getNombre());
+            statement.setString(2, comprador.getApellido());
+            statement.setString(3, comprador.getTelefono());
+            statement.setString(4, comprador.getDni());
+            statement.setString(5, comprador.getSexo());
+            statement.setString(6, comprador.getDireccion());
+            statement.setString(7, comprador.getFechaNac());
+            statement.setString(8, comprador.getEmail());
+            statement.setInt(9, 1);
+            statement.executeUpdate();
+            exito = 1;
+
+        } catch (SQLException throwables) {
+            ApiException.showException(new ApiException(ErrorCode.FAIL_SAVING_IN_DB));
+        }
+        Database.getInstance().disconnect();
+        return exito;
+    }
+
+    @Override
+    public int update(Comprador comprador) {
+        String sqlUpdate = " UPDATE Comprador SET Nombre = ?, Apellido = ?, Telefono = ?, Sexo = ?, Direccion = ?, Fecha_Nacimiento = ?, Email = ?" +
+                " VALUES (?, ?, ?, ?, ?, ?, ?) WHERE (DNI = '" + comprador.getDni() + "') AND ('" + "Sexo =" + comprador.getSexo() + "')";
+        int exito = 0;
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(sqlUpdate);
+            statement.setString(1, comprador.getNombre());
+            statement.setString(2, comprador.getApellido());
+            statement.setString(3, comprador.getTelefono());
+            statement.setString(4, comprador.getSexo());
+            statement.setString(5, comprador.getDireccion());
+            statement.setString(6, comprador.getFechaNac());
+            statement.setString(7, comprador.getEmail());
+            statement.executeUpdate();
+            exito = 1;
+
+        } catch (SQLException throwables) {
+            ApiException.showException(new ApiException(ErrorCode.FAIL_SAVING_IN_DB));
+        }
+        Database.getInstance().disconnect();
+        return exito;
+    }
+
+    @Override
+    public int delete(Comprador comprador) {
+        String sqlDelete = " UPDATE Comprador SET Status = 0 " +
+                "WHERE (DNI = '" + comprador.getDni() + "') AND ('" + "Sexo =" + comprador.getSexo() + "')";
+        int exito = 0;
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(sqlDelete);
+            statement.executeUpdate();
+            exito = 1;
+        } catch (SQLException throwables) {
+            ApiException.showException(new ApiException(ErrorCode.FAIL_SAVING_IN_DB));
+        }
+        Database.getInstance().disconnect();
+        return exito;
+    }
+}
---
+++ src/main/java/com/ingsof2/DAO/DAOVenta.java
@@ -0,0 +1,117 @@
+package com.ingsof2.DAO;
+
+import com.ingsof2.Objetos.Alquiler;
+import com.ingsof2.Objetos.Venta;
+import com.ingsof2.database.Database;
+import com.ingsof2.exceptions.ApiException;
+import com.ingsof2.utils.ErrorCode;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOVenta implements BusinessObject<Venta>{
+    @Override
+    public List<Venta> readAll() {
+        List<Venta> ventas = new ArrayList<>();
+        Connection connection = Database.getInstance().getConnection();
+
+        Statement statement;
+        try {
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM Venta WHERE (Status=1)");
+            Venta venta;
+            while (rs.next()) {
+                venta = new Venta();
+                venta.setCodigo(rs.getString("Codigo"));
+                venta.setTipo(2);//1 es alquiler, 2 es venta
+                venta.setFecha(rs.getString("Fecha_Contrato"));
+                venta.setComision(rs.getInt("Comision"));
+                venta.setDniComprador(rs.getString("DNI_Comprador"));
+                venta.setDomicilioInmueble(rs.getString("Domicilio_Inmueble"));
+                venta.setDniVendedor(rs.getString("DNI_Vendedor"));
+                ventas.add(venta);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        Database.getInstance().disconnect();
+        return ventas;
+    }
+
+    @Override
+    public Venta ReadOne(String... ids) {
+        return null;
+    }
+
+    @Override
+    public int create(Venta venta) {
+        String sqlInsert = " INSERT INTO Venta (Codigo, Tipo, Fecha_Contrato, Comision, DNI_Comprador, Domicilio_Inmueble, DNI_Vendedor, Status)" +
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        int exito = 0;
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(sqlInsert);
+            statement.setString(1, venta.getCodigo());
+            statement.setInt(2, venta.getTipo());
+            statement.setString(3, venta.getFecha());
+            statement.setInt(4, venta.getComision());
+            statement.setString(5, venta.getDniComprador());
+            statement.setString(6, venta.getDomicilioInmueble());
+            statement.setString(7, venta.getDniVendedor());
+            statement.setInt(8, 1);
+            statement.executeUpdate();
+            exito = 1;
+
+        } catch (SQLException throwables) {
+            ApiException.showException(new ApiException(ErrorCode.FAIL_SAVING_IN_DB));
+        }
+        Database.getInstance().disconnect();
+        return exito;
+    }
+
+    @Override
+    public int update(Venta venta) {
+        String sqlUpdate = " UPDATE Venta SET Tipo = ?, Fecha_Contrato = ?, Comision = ?, DNIComprador = ?, Domicilio_Inmueble = ?, DNI_Vendedor = ?" +
+                " VALUES (?, ?, ?, ?, ?, ?, ?, ?) WHERE (Codigo = '" + venta.getCodigo() + "')";
+        int exito = 0;
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(sqlUpdate);
+            statement.setInt(1, venta.getTipo());
+            statement.setString(2, venta.getFecha());
+            statement.setInt(3, venta.getComision());
+            statement.setString(5, venta.getDniComprador());
+            statement.setString(6, venta.getDomicilioInmueble());
+            statement.setString(7, venta.getDniVendedor());
+            statement.executeUpdate();
+            exito = 1;
+
+        } catch (SQLException throwables) {
+            ApiException.showException(new ApiException(ErrorCode.FAIL_SAVING_IN_DB));
+        }
+        Database.getInstance().disconnect();
+        return exito;
+    }
+
+    @Override
+    public int delete(Venta venta) {
+        String sqlDelete = " UPDATE Venta SET Status = 0 " +
+                "WHERE (Codigo = '" + venta.getCodigo() + "')";
+        int exito = 0;
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement(sqlDelete);
+            statement.executeUpdate();
+            exito = 1;
+        } catch (SQLException throwables) {
+            ApiException.showException(new ApiException(ErrorCode.FAIL_SAVING_IN_DB));
+        }
+        Database.getInstance().disconnect();
+        return exito;
+    }
+}
